f93e68be0ed7d0c336ef8f7fef2d65624c095fa4
jsonLiang <yuanyu90221@gmail.com>
2020-07-15 01:03:33 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yuanyu90221/sitemap
+
+go 1.14
+
+require github.com/yuanyu90221/link v0.0.0-20200712181220-da474989f8f1 // indirect
---
+++ main/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/yuanyu90221/link"
+)
+
+/*
+	1 Get the webpage
+	2 parse all the links on the page
+	3 build proper urls with our links
+	4 filter out any links w/a diff domain
+	5 Find all pages (BFS)
+	6 print out xml
+*/
+
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+const xmlns = "http://www.sitemaps.org/schemaa/sitemap/0.9"
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "url to parse sitemap")
+	maxDepth := flag.Int("depth", 10, "the maximum number of links deep to traverse")
+	flag.Parse()
+	pages := bfs(*urlFlag, *maxDepth)
+	// fmt.Printf("parse sitemap from %v\n", *urlFlag)
+	// pages := get(*urlFlag)
+	toXml := urlset{
+		Xmlns: xmlns,
+	}
+	for _, page := range pages {
+		toXml.Urls = append(toXml.Urls, loc{page})
+	}
+
+	fmt.Printf(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(toXml); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				if _, ok := seen[link]; !ok {
+					nq[link] = struct{}{}
+				}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+
+	if err != nil {
+		return []string{}
+	}
+	defer resp.Body.Close()
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, l := range links {
+		if keepFn(l) {
+			ret = append(ret, l)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(l string) bool {
+		return strings.HasPrefix(l, pfx)
+	}
+}
